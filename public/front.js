899a35c0c124c44efe111f745840c76dd85214e6
caiyix <95072240+caiyix@users.noreply.github.com>
2022-11-06 22:47:36 +0800

    更新html列表问题

--- public/front.js
+++ public/front.js
@@ -1,2 +1,11 @@
 let host = "http://127.0.0.1:8000"
 
+async function pass() {
+    let url = host + "/pass"
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("step0").value = data["PrivateKey"]
+            document.getElementById("step1").value = data["PublicKey"]
+        })
+}
