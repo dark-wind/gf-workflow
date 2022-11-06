da5615345102d21009fd333432e2aa037ea17a8d
dark-wind <326956600@163.com>
2022-11-06 17:49:45 +0800

    界面展示

--- internal/logic/view/view.go
+++ internal/logic/view/view.go
@@ -2,6 +2,7 @@ package view
 
 import (
 	"fmt"
+	"gf-workflow/internal/model/entity"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
 )
@@ -9,6 +10,8 @@ import (
 type View struct{}
 
 func (View) Task(r *ghttp.Request) {
+	var task entity.Tasks
+	g.Model(entity.Tasks{}).Where("id", 2).Scan(&task)
 	err := r.Response.WriteTpl("task.tpl", g.Map{
 		"id":   123,
 		"name": "john",
